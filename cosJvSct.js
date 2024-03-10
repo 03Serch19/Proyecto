0cf90300c152a6bc4276c263e0f2c989691b563b
7sersh <s.lopezdg1921@gmail.com>
2024-03-10 12:34:08 -0600

    un par de agregados al sitio web

--- cosJvSct.js
+++ cosJvSct.js
@@ -2,9 +2,10 @@
  document.addEventListener("DOMContentLoaded", function() {
   // Array de objetos de streamers y sus países
   let streamers = [
-      { nombre: "Streamer 1", pais: "País 1" },
-      { nombre: "Streamer 2", pais: "País 2" },
-      { nombre: "Streamer 3", pais: "País 3" }
+      { nombre: "Streamer 1", pais: "País 1", edad:"24" },
+      { nombre: "Streamer 2", pais: "País 2", edad:"36"},
+      { nombre: "Streamer 3", pais: "País 3", edad:"32"},
+      { nombre: "Streamer 3", pais: "País 4", edad:"19"}
       // Añade más streamers según sea necesario
   ];
 
@@ -21,8 +22,12 @@
       let paragraph = document.createElement("p");
       paragraph.textContent = "País: " + streamer.pais;
 
+      let paragraphAge = document.createElement("p");
+      paragraphAge.textContent = "Edad: " + streamer.edad;
+
       article.appendChild(heading);
       article.appendChild(paragraph);
+      article.appendChild(paragraphAge)
       streamersSection.appendChild(article);
   });
 });
